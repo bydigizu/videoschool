e8fbf870fbdd6e511be62b8dc964b7900ce966a6
Bydigizu <bydigizu@gmail.com>
2020-03-29 15:20:44 +0300

    add debug module

--- config/web.php
+++ config/web.php
@@ -1,13 +1,27 @@
 <?php
 
-return [
+ $config = [
     'id' => 'school',
     'basePath' => realpath(__DIR__ . '/../'),
+    'bootstrap' => [
+        'debug'
+    ],
     'components' => [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+        ],
+        'request' => [
+            'cookieValidationKey' => 'z;lbkdz;blkfdv;llkfdkg;zkgf;lkgvjzdf;lvkjzfdd;glkjf'
         ]
+    ],
+    'modules' => [
+        'debug' => [
+           'class' => 'yii\debug\Module',
+           'allowedIPs'=>['*']
+        ]
+        
     ]
 ];
-
+ 
+ return $config;
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace app\controllers;
+
+use Yii;
 use \yii\web\Controller;
 
 class SiteController extends Controller
