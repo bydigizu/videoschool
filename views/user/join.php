568dddbf6d15f3ebdf7102f1736ce91ed09540a0
Bydigizu <bydigizu@gmail.com>
2020-03-31 08:18:01 +0300

    describe join and login pages

--- views/user/join.php
+++ views/user/join.php
@@ -1,1 +1,13 @@
-<h1>Join us</h1>
+
+<div class="panel panel-warning">
+  <div class="panel-heading">
+    <h1 class="panel-title">Join us</h1>
+  </div>
+  <div class="panel-body">
+      name<br>
+      email<br>
+      password<br>
+      password<br>
+      [Create]
+  </div>
+</div>
--- views/user/login.php
+++ views/user/login.php
@@ -1,1 +1,10 @@
-<h1>Log in</h1>
+<div class="panel panel-info">
+  <div class="panel-heading">
+    <h1 class="panel-title">Log in</h1>
+  </div>
+  <div class="panel-body">
+      name<br>
+      password<br>
+      [Create]
+  </div>
+</div>
