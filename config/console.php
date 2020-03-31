5959a007ee498abeb9fba4946e9aa72145290632
Bydigizu <bydigizu@gmail.com>
2020-03-31 15:42:22 +0300

    add migration for user table

--- config/console.php
+++ config/console.php
@@ -2,7 +2,15 @@
 $config = [
     'id' => 'school-console',
     'basePath' => dirname(__DIR__ ),
-    
+    'components' => [
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'mysql:host=localhost;dbname=school',
+                'username' => 'root',
+                'password' => '',
+                'charset' => 'utf8',
+            ]
+        ]
 ];
  
  return $config;
