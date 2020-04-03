55eaca810a9bdbfc9be8c5d7b4b4f5d8206c316e
Bydigizu <bydigizu@gmail.com>
2020-04-03 11:33:48 +0300

    Add function test with yii module - working with model

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return[
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=school',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=school',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
